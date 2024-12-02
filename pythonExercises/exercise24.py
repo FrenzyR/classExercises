9a74c11a6b9b5ee9decc8aeb8cd07caaa8192689
FrenzyR <rubenverrod@gmail.com>
2024-12-02 09:46:24 +0100

    Did exercise 25, which was to make a for that summed the previous numbers up to the one selected by you

--- pythonExercises/exercise24.py
+++ pythonExercises/exercise24.py
@@ -1,5 +1,11 @@
 print("Give me a positive number")
+
 user_input = int(input())
+
+while user_input < 0:
+    print("Give me a positive number")
+    user_input = int(input())
+
 def numberSum(n):
     return n * (n + 1) / 2
 
